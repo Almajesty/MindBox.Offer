2e91accc6e3b07b0d5d21f5817d2a81a9ce0b3c1
rand <m1k4z@yandex.ru>
2022-07-03 16:41:59 +0300

    Add tests and triangle

---
+++ MindBox.Offer/Classes/Triangle.cs
@@ -0,0 +1,77 @@
+﻿using static System.Math;
+
+namespace MindBox.Offer.Classes;
+
+public class Triangle : Shape
+{
+    public double Side1 { get; set; }
+    public double Side2 { get; set; }
+    public double Side3 { get; set; }
+
+    public Triangle(double s1, double s2, double s3)
+    {
+        Side1 = s1;
+        Side2 = s2;
+        Side3 = s3;
+    }
+
+    public bool CheckPythTriangle()
+    {
+        // If triangle is right it's sides complete Pythagorean theorem
+        return Math.Pow(Side1, 2) == Math.Pow(Side2, 2) + Math.Pow(Side3, 2) ||
+               Math.Pow(Side2, 2) == Math.Pow(Side1, 2) + Math.Pow(Side3, 2) ||
+               Math.Pow(Side3, 2) == Math.Pow(Side1, 2) + Math.Pow(Side2, 2);
+    }
+
+    public bool ValidationInputValue()
+    {
+        if (IsNotPositive())
+        {
+            throw new Exception("All sides must be positive");
+        }
+
+        if (IsNotCorrectSideLength())
+        {
+            throw new Exception("For any triangle two sides must be more then one side");
+        }
+
+        return true;
+    }
+
+    public bool IsNotCorrectSideLength()
+    {
+        return Side1 + Side2 <= Side3 ||
+               Side1 + Side3 <= Side2 ||
+               Side2 + Side3 <= Side1;
+    }
+
+    public bool IsNotPositive()
+    {
+        return Side1 <= 0 || Side2 <= 0 || Side3 <= 0;
+    }
+
+    public double Perimeter
+    {
+        get
+        {
+            return Side1 + Side2 + Side3;
+        }
+    }
+
+    /// <summary>
+    /// Use Heron's formula
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public double GetAreaUseHeronsFormula()
+    {
+        var semiPerimeter = Perimeter / 2;
+
+        return Sqrt(semiPerimeter * (semiPerimeter - Side1) * (semiPerimeter - Side2) * (semiPerimeter - Side3));
+    }
+
+    public override double Area()
+    {
+        return GetAreaUseHeronsFormula();
+    }
+}
---
+++ OfferUnitTests/TriangleTests.cs
@@ -0,0 +1,81 @@
+﻿using MindBox.Offer.Classes;
+using Xunit;
+
+namespace MindBox.OfferUnitTests
+{
+    public class TriangleTests
+    {
+        /// <summary>
+        /// Если стороная отрицательной длины вернуть true
+        /// </summary>
+        [Fact]
+        public void Triangle_HaveNegativeSide_IsNotPositive()
+        {
+            // arrange
+            var triangle = new Triangle(-1, 2, 2);
+            // act
+            var result = triangle.IsNotPositive();
+            // assert
+            Assert.True(result);
+        }
+
+        /// <summary>
+        /// Если две стороны меньше третьей вернуть true
+        /// </summary>
+        [Fact]
+        public void Triangle_IsNotCorrectSideLength()
+        {
+            // arrange
+            var triangle = new Triangle(35, 3, 10);
+            // act
+            var result = triangle.IsNotCorrectSideLength();
+            // assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void Triangle_CheckZeroSize_ValidationInputValue()
+        {
+            // arrange
+            var triangle = new Triangle(0, 3, 10);
+            // act & assert
+            var result = Assert.Throws<Exception>(() => triangle.ValidationInputValue());
+        }
+
+        [Fact]
+        public void Triangle_3_2_2_CheckPythTriangle_ReturnFalse()
+        {
+            // arrange
+            var triangle = new Triangle(3, 2, 2);
+            var expected = false;
+            // act
+            var result = triangle.CheckPythTriangle();
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Triangle_3_4_5_CheckPythTriangle_ReturnTrue()
+        {
+            // arrange
+            var triangle = new Triangle(3, 4, 5);
+            var expected = true;
+            // act
+            var result = triangle.CheckPythTriangle();
+            // assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Triangle_CheckPythTriangle_6_8_10_GetArea()
+        {
+            // arrange
+            var triangle = new Triangle(6, 8, 10);
+            var expected = 24;
+            // act
+            var result = triangle.GetAreaUseHeronsFormula();
+            // assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
